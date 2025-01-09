4845fb7f2886f57e54bc77693abe40c6bf56afa7
magae1 <jjilil8351@gmail.com>
2025-01-10 00:46:49 +0900

    🎉 하나학당 웹소켓 서버 프로젝트 시작

---
+++ src/main/java/com/hanahakdangwebsocketserver/HanaHakdangWebsocketServerApplication.java
@@ -0,0 +1,13 @@
+package com.hanahakdangwebsocketserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanaHakdangWebsocketServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HanaHakdangWebsocketServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/hanahakdangwebsocketserver/hanahakdangwebsocketserver/HanaHakdangWebsocketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanahakdangwebsocketserver.hanahakdangwebsocketserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanaHakdangWebsocketServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
