5a3fb254702ac619f609f0959359b334dcd865fe
magae1 <jjilil8351@gmail.com>
2025-01-13 10:43:08 +0900

    🔧 docker 스크립트 추가

--- deploy/run-coturn.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker run -d -p 3478:3478 -p 3478:3478/udp \
-              -p 5349:5349 -p 5349:5349/udp \
-              -p 49152-65535:49152-65535/udp \
-              coturn/coturn:4.6.3-alpine3.21
